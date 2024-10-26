60a180687852e9acb63a0434c0f4f2240f64104a
premdeore <prem.developer69@gmail.com>
2024-10-27 01:52:50 +0530

    added logo upload feature and multer

--- controllers/company/company.controller.js
+++ controllers/company/company.controller.js
@@ -1,4 +1,6 @@
 import { Company } from "../../models/company.model.js";
+import cloudinary from "../../utils/cloudinary.js";
+import getDataUri from "../../utils/datauri.js";
 
 
 export const registerCompany = async(req, res)=>{
@@ -41,9 +43,9 @@ export const getCompany = async(req, res)=>{
     try {
         const userId = req.id;  // loggedin User Id
 
-        const comapanies = await Company.find({userId});
+        const companies = await Company.find({userId});
 
-        if(!comapanies){
+        if(!companies){
             return res.status(404).json({
                 message:"Companies not found.",
                 success: false
@@ -51,7 +53,7 @@ export const getCompany = async(req, res)=>{
         }
 
         return res.status(200).json({
-            comapanies,
+            companies,
             success: true
         })
 
@@ -88,8 +90,11 @@ export const updateCompany = async(req, res)=>{
         const {name, description, website, location} = req.body;
         const file = req.file;
         //cloudinary
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
 
-        const updateData = {name, description, website, location}
+        const updateData = {name, description, website, location , logo}
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData,{new:true});
         if(!company){
